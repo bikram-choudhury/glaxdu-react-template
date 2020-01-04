5869cfc65eed52408efc2649ba37a3ef6225e45c
Bikram Choudhury <bikram.choudhury527@gmail.com>
2020-01-04 12:49:25 +0800

    Register form using Formik

--- src/components/AuthenticateForm/AuthenticateForm.jsx
+++ src/components/AuthenticateForm/AuthenticateForm.jsx
@@ -3,7 +3,7 @@ import TopHeader from '../TopHeader/TopHeader.jsx';
 import BrnadLogoArea from '../BrnadLogo/BrnadLogoArea.jsx';
 import Footer from '../Footer.jsx';
 
-import { Formik, Field } from 'formik';
+import { Formik, Field, Form } from 'formik';
 import Axios from 'axios';
 import { JSON_PLACEHOLDER_API } from '../../glaxdu-settings';
 
@@ -73,67 +73,75 @@ class AuthenticationContainer extends Component {
     }
 }
 
-class LoginForm extends Component {
-
-    handleLogin = (formData, { setSubmitting }) => {
-        setSubmitting(true);
-        console.log(formData);
-        if (formData && Object.keys(formData).length) {
-            const URL = `${JSON_PLACEHOLDER_API}/users`;
-            Axios.post(URL, formData)
-                .then(response => {
-                    console.log(response);
-                    setSubmitting(false);
-                })
-                .catch(error => console.error(error))
-        }
-    }
-    render() {
-        const initialLoginFormValue = { username: '', password: '', rememberUser: false };
-        return (
-            <div className="login-form-container">
-                <div className="login-register-form">
-                    <Formik initialValues={initialLoginFormValue} onSubmit={this.handleLogin}>
-                        {({ isSubmitting, values, handleChange, handleBlur, handleSubmit }) => (
-                            <form onSubmit={handleSubmit}>
-                                <Field type="input" name="username" placeholder="Username" />
-                                <Field type="password" name="password" placeholder="Password" />
-                                {/* <input type="text" name="username" placeholder="Username"
-                                    value={values.username} onChange={handleChange} onBlur={handleBlur} /> */}
-                                <div className="button-box">
-                                    <div className="login-toggle-btn">
-                                        <Field type="checkbox" name="rememberUser" placeholder="Username" />
-                                        <label>Remember me</label>
-                                        <a href="#">Forgot Password?</a>
-                                    </div>
-                                    <button disabled={isSubmitting} className="default-btn" type="submit"><span>Login</span></button>
+function LoginForm(props) {
+    const initialLoginFormValue = { username: '', password: '', rememberUser: false };
+    return (
+        <div className="login-form-container">
+            <div className="login-register-form">
+                <Formik initialValues={initialLoginFormValue} onSubmit={handleLoginAndRegistration}>
+                    {({ isSubmitting, values, handleSubmit }) => (
+                        <form onSubmit={handleSubmit}>
+                            <Field type="input" name="username" placeholder="Username" />
+                            <Field type="password" name="password" placeholder="Password" />
+                            {/* <input type="text" name="username" placeholder="Username"
+                                value={values.username} onChange={handleChange} onBlur={handleBlur} /> */}
+                            <div className="button-box">
+                                <div className="login-toggle-btn">
+                                    <Field type="checkbox" name="rememberUser" placeholder="Username" />
+                                    <label>Remember me</label>
+                                    <a href="#">Forgot Password?</a>
                                 </div>
-                                <pre>{JSON.stringify(values, '', 3)}</pre>
-                            </form>
-                        )}
-                    </Formik>
-
-                </div>
+                                <button disabled={isSubmitting} className="default-btn" type="submit"><span>Login</span></button>
+                            </div>
+                        </form>
+                    )}
+                </Formik>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
-class RegisterForm extends Component {
-    render() {
-        return (
-            <div className="login-form-container">
-                <div className="login-register-form">
-                    <form action="#" method="post">
-                        <input type="text" name="user-name" placeholder="Username" />
-                        <input type="password" name="user-password" placeholder="Password" />
-                        <input name="user-email" placeholder="Email" type="email" />
-                        <div className="button-box">
-                            <button className="default-btn" type="submit"><span>Register</span></button>
-                        </div>
-                    </form>
-                </div>
+function RegisterForm() {
+    const initialRegisterFormValue = { username: '', password: '', email: '' };
+    return (
+        <div className="login-form-container">
+            <div className="login-register-form">
+                <Formik initialValues={initialRegisterFormValue} onSubmit={handleLoginAndRegistration}>
+                    {({ isSubmitting, values }) => (
+                        <Form>
+                            <Field type="input" name="username" placeholder="Username" />
+                            <Field type="password" name="password" placeholder="Password" />
+                            <Field type="email" name="email" placeholder="Email" />
+                            <Field as="select" name="country">
+                                <option value="">Choose ...</option>
+                                <option value="india">India</option>
+                                <option value="africa">Africa</option>
+                                <option value="england">England</option>
+                                <option value="australia">Australia</option>
+                            </Field>
+                            <div className="button-box">
+                                <button disabled={isSubmitting} className="default-btn" type="submit"><span>Register</span></button>
+                            </div>
+                            <pre>{JSON.stringify(values, '', 3)}</pre>
+                        </Form>
+                    )}
+                </Formik>
+
             </div>
-        );
+        </div>
+    );
+}
+
+function handleLoginAndRegistration(formData, { setSubmitting }) {
+    setSubmitting(true);
+    console.log(formData);
+    if (formData && Object.keys(formData).length) {
+        const URL = `${JSON_PLACEHOLDER_API}/users`;
+        Axios.post(URL, formData)
+            .then(response => {
+                console.log(response);
+                setSubmitting(false);
+            })
+            .catch(error => console.error(error))
     }
 }
