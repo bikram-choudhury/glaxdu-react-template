d9aa8ec247ca2cbe48ea829f4ffb52d14bbf19b4
Bikram Choudhury <bikram.choudhury527@gmail.com>
2020-01-06 17:52:06 +0800

    [IMPL] connect store to redux-devtool

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { NewsReducer } from './reducers/news.reducer';
 
 const allReducers = combineReducers({ news: NewsReducer });
-export const store = createStore(allReducers, applyMiddleware(thunk));
+export const store = createStore(allReducers, composeWithDevTools(applyMiddleware(thunk)));
