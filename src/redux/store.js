de88d9bcd73c8efa3187bcd38f58383dd3541174
Bikram Choudhury <bikram.choudhury527@gmail.com>
2020-01-23 18:12:16 +0800

    link redux store to sessionStorage to persist the data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { loadState, saveState } from './storage';
 import thunk from 'redux-thunk';
 import { NewsReducer } from './reducers/news.reducer';
 
+const persistedState = loadState();
 const allReducers = combineReducers({ news: NewsReducer });
-export const store = createStore(allReducers, composeWithDevTools(applyMiddleware(thunk)));
+export const store = createStore(allReducers, persistedState, composeWithDevTools(applyMiddleware(thunk)));
+
+store.subscribe(() => {
+    saveState(store.getState());
+});
